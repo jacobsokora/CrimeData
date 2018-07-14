1b5d76c16abd8a94755a5daa5b53feb6180f23aa
Jacob <jacobsokora@gmail.com>
2018-07-14 14:37:45 -0600

    Add files

---
+++ CrimeData/CrimeStats.cs
@@ -0,0 +1,148 @@
+﻿using System;
+namespace CrimeData
+{
+    public class CrimeStats
+    {
+        public CrimeStats(int year, int population, int violentCrime, int murder, int rape, int robbery, int aggravatedAssault, int propertyCrime, int burglary, int theft, int motorTheft)
+        {
+            Year = year;
+            Population = population;
+            ViolentCrime = violentCrime;
+            Murder = murder;
+            Rape = rape;
+            Robbery = robbery;
+            AggravatedAssault = aggravatedAssault;
+            PropertyCrime = propertyCrime;
+            Burglary = burglary;
+            Theft = theft;
+            MotorTheft = motorTheft;
+        }
+
+        public int Year
+        {
+            get;
+        }
+
+        public int Population
+        {
+            get;
+        }
+
+        public int ViolentCrime
+        {
+            get;
+        }
+
+        public double ViolentCrimePerCapita
+        {
+            get 
+            {
+                return ViolentCrime / (Population / 100000);
+            }
+        }
+
+        public int Murder
+        {
+            get;
+        }
+
+        public double MurderPerCapita
+        {
+            get
+            {
+                return Murder / (Population / 100000);
+            }
+        }
+
+        public int Rape
+        {
+            get;
+        }
+
+        public double RapePerCapita
+        {
+            get 
+            {
+                return Rape / (Population / 100000);
+            }
+        }
+
+        public int Robbery
+        {
+            get;
+        }
+
+        public double RobberyPerCapita
+        {
+            get
+            {
+                return Robbery / (Population / 100000);
+            }
+        }
+
+        public int AggravatedAssault
+        {
+            get;
+        }
+
+        public double AggravatedAssaultPerCapita
+        {
+            get
+            {
+                return AggravatedAssault / (Population / 100000);
+            }
+        }
+
+        public int PropertyCrime
+        {
+            get;
+        }
+
+        public double PropertyCrimePerCapita
+        {
+            get
+            {
+                return PropertyCrime / (Population / 100000);
+            }
+        }
+
+        public int Burglary
+        {
+            get;
+        }
+
+        public double BurglaryPerCapita
+        {
+            get
+            {
+                return Burglary / (Population / 100000);
+            }
+        }
+
+        public int Theft
+        {
+            get;
+        }
+
+        public double TheftPerCapita
+        {
+            get
+            {
+                return Theft / (Population / 100000);
+            }
+        }
+
+        public int MotorTheft
+        {
+            get;
+        }
+
+        public double MotorTheftPerCapita
+        {
+            get
+            {
+                return MotorTheft / (Population / 100000);
+            }
+        }
+    }
+}
---
+++ CrimeData/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace CrimeData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var crimeStats = new List<CrimeStats>();
+
+            using (var streamReader = new StreamReader("CrimeData.csv"))
+            {
+                //The first line of the file is column names so let's skip it
+                streamReader.ReadLine();
+                while (!streamReader.EndOfStream)
+                {
+                    var line = streamReader.ReadLine();
+                    var parts = line.Split(",");
+                    var year = int.Parse(parts[0]);
+                    var population = int.Parse(parts[1]);
+                    var violentCrime = int.Parse(parts[2]);
+                    var murder = int.Parse(parts[3]);
+                    var rape = int.Parse(parts[4]);
+                    var robbery = int.Parse(parts[5]);
+                    var aggravatedAssault = int.Parse(parts[6]);
+                    var propertyCrime = int.Parse(parts[7]);
+                    var burglary = int.Parse(parts[8]);
+                    var theft = int.Parse(parts[9]);
+                    var motorTheft = int.Parse(parts[10]);
+                    var stat = new CrimeStats(year, population, violentCrime, murder, rape, robbery, aggravatedAssault, propertyCrime, burglary, theft, motorTheft);
+                    crimeStats.Add(stat);
+                }
+            }
+
+            var query = from stat in crimeStats orderby stat.Burglary ascending select stat.Year;
+            foreach (var item in query)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
